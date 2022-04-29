85c6971a46748ab3d7da8eb4c082de0b117800b4
Nate <natelharrison@gmail.com>
2022-04-29 11:36:58 -0700

    Revering to previous commit

--- plts.py
+++ plts.py
@@ -1,17 +1,15 @@
-from pathlib import Path
-import imagej
-import time
-import argparse
-import tifffile
+from glob import glob
+import imagej, time, argparse
 import numpy as np
 import imagej.dims as dims
-
+import xarray as xr
 
 
 fetch_interval = 10
-directory = Path('C:/Users/Viz4/Desktop/nate-pyimagej/MIPs')
+directory = 'MIPs'
 channel_list = ['CamA_ch0']
 
+<<<<<<< HEAD
 def add_image(file_list):
     print(file_list)
     tif_list = []
@@ -21,6 +19,35 @@ def add_image(file_list):
     new_dataset_4d = ij.py.to_java(timelapse)
     ij.ui().show(new_dataset_4d)
     time.sleep(10)
+=======
+def fetch_files():
+    old_files = []
+    new_files = []
+        
+    for file in sorted(glob(directory+'/*tif')):
+        if file not in old_files:
+            new_files.append(file)
+            old_files.append(file)
+
+    for channel in channel_list:
+        file_list = []
+        for file in new_files:
+            if channel in file:
+                file_list.append(file)
+        add_image(file_list)
+
+def add_image(file_list):
+	print(file_list)
+	dataset_4d = ij.io().open(file_list[0])
+	xarr_4d = ij.py.from_java(dataset_4d)
+	print(f"xarr/numpy shape: {xarr_4d.shape}")
+	for file in file_list[1:]:
+		print(file)
+		dataset_2d = ij.io().open(file)
+		xarr_2d = ij.py.from_java(dataset_2d)
+		xarr_4d = xr.concat((xarr_2d, xarr_4d), dim='time')
+		print(f"xarr/numpy shape: {xarr_4d.shape}")
+>>>>>>> parent of 48907e0 (Pathlib and zip update!)
 
 def fetch_files():
     while True:
@@ -41,7 +68,5 @@ def fetch_files():
 	        add_image(file_list)
 
 # initialize ImageJ
-ij = imagej.init(mode='interactive')
-print(f"ImageJ version: {ij.getVersion()}")
-ij.ui().showUI()
-add_image(fetch_files())
+ij = imagej.init('net.imagej:imageJ:2.1.0')	
+fetch_files()
