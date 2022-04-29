3f8e8b5ddcce067d2cc71bd066197e730f97f6df
Nate <natelharrison@gmail.com>
2022-04-29 11:16:49 -0700

    Test of live file fetching

--- plts.py
+++ plts.py
@@ -12,23 +12,6 @@ fetch_interval = 10
 directory = Path('C:/Users/Viz4/Desktop/nate-pyimagej/MIPs')
 channel_list = ['CamA_ch0']
 
-def fetch_files():
-    old_files = set()
-    new_files = set()
-
-    for file in sorted(directory.glob(zip('CamA', 'CamB')+'.tif')):
-        print(file)
-        if file not in old_files:
-            new_files.add(file)
-            old_files.add(file)
-
-    for channel in channel_list:
-        file_list = []
-        for file in new_files:
-            if channel in file:
-                file_list.append(file)
-        return file_list
-
 def add_image(file_list):
     print(file_list)
     tif_list = []
@@ -37,8 +20,25 @@ def add_image(file_list):
     timelapse = np.asarray(tif_list)
     new_dataset_4d = ij.py.to_java(timelapse)
     ij.ui().show(new_dataset_4d)
-    time.sleep(100)
+    time.sleep(10)
 
+def fetch_files():
+    while True:
+	    old_files = set()
+	    new_files = set()
+
+	    for file in sorted(directory.glob(zip('CamA', 'CamB')+'.tif')):
+	        print(file)
+	        if file not in old_files:
+	            new_files.add(file).difference_update(old_files)
+	            old_files.add(file)
+
+	    for channel in channel_list:
+	        file_list = []
+	        for file in new_files:
+	            if channel in file:
+	                file_list.append(file)
+	        add_image(file_list)
 
 # initialize ImageJ
 ij = imagej.init(mode='interactive')
